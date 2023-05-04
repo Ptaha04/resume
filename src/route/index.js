6ecc6a049090b6c326b56f36b39eb811b88a3a4f
Ptaha04 <voronaw@gmail.com>
2023-05-04 21:19:25 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,35 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '$600 per month',
+  address: 'Default City',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'ivanov@mail.com',
+      href: 'mailto:ivanov@mail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      href: 'https://www.linkedin.com/in/dmytro-test',
+      text: 'LinkedIn',
+    },
+  },
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -23,10 +52,251 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Summary',
+    },
+
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: 'Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.',
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics ( tournament position, goals etc), analyzing bysimple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.',
+      },
+    },
+
+    footer,
   })
 })
 
 // ================================================================
 
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: 8,
+          isTop: true,
+        },
+        {
+          name: 'VS Code',
+          point: 6,
+          isTop: false,
+        },
+        {
+          name: 'GIT',
+          point: 10,
+        },
+        {
+          name: 'Terminal',
+          point: 9,
+        },
+        {
+          name: 'NPM',
+          point: 7,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+      hobbies: [
+        {
+          name: 'Skiing',
+          isMain: true,
+        },
+        {
+          name: 'Riding',
+          isMain: false,
+        },
+        {
+          name: 'Fishing',
+          isMain: true,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+
+    main: {
+      educations: [
+        {
+          name: 'National Technical University of Ukraine «Igor Sikorsky Kyiv Polytechnic Institute»',
+          isEnd: false,
+        },
+        {
+          name: 'Kiev National University of Civil Engineering and Architecture',
+          isEnd: true,
+        },
+        {
+          name: 'National University of Food Technologies',
+          isEnd: true,
+        },
+        {
+          name: 'Kyiv Banking Institute',
+          isEnd: false,
+        },
+        {
+          name: 'Kyiv International University',
+          isEnd: true,
+        },
+        {
+          name: 'Kyiv National Linguistic University',
+          isEnd: false,
+        },
+        {
+          name: 'Kyiv National University of Culture and Art',
+          isEnd: true,
+        },
+        {
+          name: 'Taras Shevchenko National University of Kyiv',
+          isEnd: false,
+        },
+        {
+          name: 'National Agricultural University',
+          isEnd: true,
+        },
+        {
+          name: 'Bogomolets National Medical University',
+          isEnd: false,
+        },
+        {
+          name: 'Taras Shevchenko National University of Kyiv',
+          isEnd: true,
+        },
+      ],
+
+      certificates: [
+        {
+          name: 'FCO',
+          id: 12,
+        },
+        {
+          name: 'WBO',
+          id: 8,
+        },
+        {
+          name: 'NHL',
+          id: 9,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior fullstack deweloper',
+          company: {
+            name: 'IT Brains',
+            // url: 'https://it-brains.com.ua/',
+            url: null,
+          },
+          duration: {
+            from: '10.10.2022',
+            to: null,
+          },
+          projectAmount: 3,
+
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://resume.com.ua',
+              about:
+                'Airbnb competitor. kjnkjnfd kjnkjng kjnm,n.,ad',
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Nodejs',
+                },
+              ],
+              awards: [
+                {
+                  name: 'Background kfhdkh  kjhlk ariuri eru',
+                },
+                {
+                  name: 'Prepearing 2896748 276782980 23894 4890',
+                },
+              ],
+              stackAmount: 7,
+              awardAmount: 9,
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
+  })
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
